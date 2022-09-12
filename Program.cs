bba4f36def36226526bfe71172a504ca74503081
Cara Netzer <cara.netzer@gmail.com>
2022-09-12 14:06:51 -0400

    after phase 8

--- Program.cs
+++ Program.cs
@@ -11,8 +11,62 @@ namespace EnthusiasticMoose
             Console.WriteLine(@"@ multi-line string: Welcome to the Enthusiastic Moose Simulator!
 --------------------------------------------");
 
-            MooseSays("H I, I'M  E N T H U S I A S T I C !");
-            MooseSays("I really am enthusiastic");
+            //MooseSays("H I, I'M  E N T H U S I A S T I C !");
+            //MooseSays("I really am enthusiastic");
+
+            AskQuestion("Is Canada real?");
+            AskQuestion("Are you enthusiastic?");
+            AskQuestion("Do you love C# yet?");
+            AskQuestion("Do you want to know a secret?");
+        }
+
+        static void AskQuestion(string question)
+        {
+            Console.Write($"{question} (Y/N): ");
+            string answer = Console.ReadLine().ToLower();
+
+            while (answer != "y" && answer != "n")
+            {
+                Console.Write($"{question} (Y/N): ");
+                answer = Console.ReadLine().ToLower();
+            }
+
+            if (answer == "y" && question.Contains("Canada"))
+            {
+                MooseSays("Really? It seems very unlikely.");
+            }
+            else if (answer == "y" && question.Contains("enthusiastic"))
+            {
+                MooseSays("Yay!");
+            }
+            else if (answer == "y" && question.Contains("C#"))
+            {
+                MooseSays("Good job sucking up to your instructor!");
+            }
+            else if (answer == "y" && question.Contains("secret"))
+            {
+                MooseSays("ME TOO!!!! I love secrets...tell me one!");
+            }
+            else if (answer == "n" && question.Contains("Canada"))
+            {
+                MooseSays("I  K N E W  I T !!!");
+            }
+            else if (answer == "n" && question.Contains("enthusiastic"))
+            {
+                MooseSays("Yay!");
+            }
+            else if (answer == "n" && question.Contains("C#"))
+            {
+                MooseSays("You will...oh, yes, you will...");
+            }
+            else if (answer == "n" && question.Contains("secret"))
+            {
+                MooseSays("Oh, no...secrets are the best, I love to share them!");
+            }
+            else
+            {
+                MooseSays("I don't know that one");
+            }
         }
 
         static void MooseSays(string message)
@@ -45,6 +99,6 @@ namespace EnthusiasticMoose
                       \ \'._  ` '_.'
                        `^^` `^^^`
             ");
+        }
     }
-}
 }
