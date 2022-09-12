41628851800f1c9ed4cc1296412b2d8f69232141
Cara Netzer <cara.netzer@gmail.com>
2022-09-12 11:46:39 -0400

    after phase 3

--- Program.cs
+++ Program.cs
@@ -6,7 +6,44 @@ namespace EnthusiasticMoose
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!" + Environment.NewLine + "--------------------------------------------");
+            Console.WriteLine("Environment.NewLine: Welcome to the Enthusiastic Moose Simulator!" + Environment.NewLine + "--------------------------------------------");
+            Console.WriteLine("Escape Characters: Welcome to the Enthusiastic Moose Simulator!\n--------------------------------------------\n");
+            Console.WriteLine(@"@ multi-line string: Welcome to the Enthusiastic Moose Simulator!
+--------------------------------------------");
+
+            MooseSays();
         }
+
+        static void MooseSays()
+        {
+            Console.WriteLine(@"
+                                       _.--^^^--,
+                                    .'          `\
+  .-^^^^^^-.                      .'              |
+ /          '.                   /            .-._/
+|             `.                |             |
+ \              \          .-._ |          _   \
+  `^^'-.         \_.-.     \   `          ( \__/
+        |             )     '=.       .,   \
+       /             (         \     /  \  /
+     /`               `\        |   /    `'
+     '..-`\        _.-. `\ _.__/   .=.
+          |  _    / \  '.-`    `-.'  /
+          \_/ |  |   './ _     _  \.'
+               '-'    | /       \ |
+                      |  .-. .-.  |
+                      \ / o| |o \ /
+                       |   / \   |    H I, I'M  E N T H U S I A S T I C !
+                      / `^`   `^` \
+                     /             \
+                    | '._.'         \
+                    |  /             |
+                     \ |             |
+                      ||    _    _   /
+                      /|\  (_\  /_) /
+                      \ \'._  ` '_.'
+                       `^^` `^^^`
+            ");
     }
+}
 }
